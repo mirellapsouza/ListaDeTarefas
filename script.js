7118de98462d6cf2fec2ffc91c1ae1d28beed314
mirellapsouza <pszmirella@gmail.com>
2024-11-15 21:28:36 -0300

    .
    add botao tarefa concluida

--- script.js
+++ script.js
@@ -2,18 +2,26 @@ const lista = document.getElementById("lista");
 const input = document.getElementById("input");
 
 function addTarefa() {
-    if (input !== "") {
+    if (input.value.trim() !== "") {
         const tarefa = document.createElement("li");
         tarefa.innerHTML = `
             <span>${input.value}</span>
-             <button class="editarBtn" onClick="editarTarefa(this)">Editar</button> 
-             <button class="deleteBtn" onClick="deletarTarefa(this)">Deletar</button>
+            <button class="checkBtn" onClick="checkTarefa(this)">✔</button>
+            <button class="editarBtn" onClick="editarTarefa(this)">✎</button> 
+            <button class="deleteBtn" onClick="deletarTarefa(this)">✘</button>
          `
          lista.appendChild(tarefa);
          input.value = "";
         
     }
 }
+
+function checkTarefa(button) {
+    const tarefa = button.parentElement; //get li
+    tarefa.classList.toggle("concluida"); //acrescenta a classe pra modificar estilo
+    button.classList.toggle("active"); //permite refazer a tarefa
+    lista.appendChild(tarefa); //leva a tarefa ao final da lista, mas ela fica lá...
+    }
 
 function editarTarefa(button) {
     const tarefa = button.parentElement;
